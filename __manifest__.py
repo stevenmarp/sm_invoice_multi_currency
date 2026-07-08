088a97c1b6fd23c398ec3454a73945cb6f93b787
stevenmarp <100309799+stevenmarp@users.noreply.github.com>
2026-07-08 18:14:16 +0700

    port: compatible with Odoo 14.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Invoice Multi Currency',
-    'version': '15.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Accounting',
     'summary': 'Show invoice amounts in both invoice currency and company currency',
     'description': """
